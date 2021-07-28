8d21c8ef0ca1f5f8a8d17f5531c4fcbd7b9a12bc
Sanya Gera <75986201+SanyaGera@users.noreply.github.com>
2021-07-28 02:49:19 -0700

    Create Power - Mod Power.py

---
+++ Power - Mod Power.py
@@ -0,0 +1,9 @@
+#Problem link:https://www.hackerrank.com/challenges/python-power-mod-power/problem?h_r=next-challenge&h_v=zen
+#Problem name:Power - Mod Power
+#Status:Easy
+#Solution
+a=int(input())
+b=int(input())
+m=int(input())
+print(pow(a,b))
+print(pow(a,b,m))
