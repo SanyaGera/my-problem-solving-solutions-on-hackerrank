c5aaf99a363d79222b8cfe566874331f2b25ee1d
Sanya Gera <75986201+SanyaGera@users.noreply.github.com>
2021-07-28 02:41:57 -0700

    Create Mod Divmod.py

---
+++ Mod Divmod.py
@@ -0,0 +1,9 @@
+#Problem link:https://www.hackerrank.com/challenges/python-mod-divmod/problem
+#Problem name:Mod Divmod
+#Status:Easy
+#Solution
+a=int(input())
+b=int(input())
+print(a//b)
+print(a%b)
+print(divmod(a,b))
