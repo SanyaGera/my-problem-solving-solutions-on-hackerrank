d55925a268fce12734c9d7ba4de9782b29bd5d18
Sanya Gera <75986201+SanyaGera@users.noreply.github.com>
2021-07-28 05:08:26 -0700

    Create Triangle Quest.py

---
+++ Triangle Quest.py
@@ -0,0 +1,6 @@
+#Problem link:https://www.hackerrank.com/challenges/python-quest-1/problem
+#Problem name:Triangle Quest
+#Status: Easy
+#Solution
+for  i in range(1,int(input())):
+    print((10**i//9)*i)
