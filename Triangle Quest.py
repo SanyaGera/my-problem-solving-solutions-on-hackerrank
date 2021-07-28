09a6743ce2df7de4e61dffe2dae97e59f790b5b0
Sanya Gera <75986201+SanyaGera@users.noreply.github.com>
2021-07-28 08:22:07 -0700

    Update Triangle Quest.py

--- Triangle Quest.py
+++ Triangle Quest.py
@@ -1,6 +1,6 @@
 #Problem link:https://www.hackerrank.com/challenges/python-quest-1/problem
 #Problem name:Triangle Quest
-#Status: Easy
+#Status: Medium
 #Solution
 for  i in range(1,int(input())):
     print((10**i//9)*i)
