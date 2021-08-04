4c15b875393d9e413881a866717ed3b694e39154
Sanya Gera <75986201+SanyaGera@users.noreply.github.com>
2021-08-04 04:01:01 -0700

    Create String Construction.py

---
+++ String Construction.py
@@ -0,0 +1,7 @@
+#Problem link:https://www.hackerrank.com/challenges/string-construction/problem
+#Problem name:String Construction
+#Status:Easy
+#Solution
+def stringConstruction(s):
+    s=set(s)
+    return (len(s))
